0f9077f35778f75cae2eef1c0d71157e8f57735a
Mikhail Afonin <afoninmv@gmail.com>
2022-06-19 23:51:08 +0300

    refactor: remove useless code

--- src/__tests__/first.test.ts
+++ src/__tests__/first.test.ts
@@ -1,7 +1,7 @@
-import { age } from '../index';
+// import { server } from '../server';
 
-describe('This is a test', () => {
-	it('just for test', () => {
-		expect(age).toBe(99);
-	});
-});
+// describe('First scenario: GET /api/users', () => {
+// 	it("Should be one user: {id: '123e4567-e89b-12d3-a456-426614174000', userName: 'Alex', age: 28, hobbies: []}", () => {
+
+// 	});
+// });
--- src/index.ts
+++ src/index.ts
@@ -4,22 +4,4 @@ import server from './server';
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
-
-console.log('process.env.PORT: ', PORT);
-
-type TypeAge = number;
-
-export const age: TypeAge = 99;
-
-export const b = {
-	a: 4,
-	b: 3
-};
-
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
-// Test comment
-
-console.log(age);
-
-console.log(b);
